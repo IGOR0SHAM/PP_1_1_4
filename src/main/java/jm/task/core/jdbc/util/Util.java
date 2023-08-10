c99717101220c6d3e560e6c0835a5b3afec322a4
IGOR0SHAM <igor.sgride@yandex.ru>
2023-08-10 23:27:40 +0500

    commit

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,24 @@
 package jm.task.core.jdbc.util;
 
+import com.mysql.cj.jdbc.Driver;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/users";
+    private static final String USERNAME = "sham";
+    private static final String PASS = "root10";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try{
+            Driver driver = new Driver();
+            DriverManager.registerDriver(driver);
+            connection = DriverManager.getConnection(URL, USERNAME, PASS);
+        } catch(SQLException e) {
+            System.err.println("Dont load driver SQL");
+        }
+        return connection;
+    }
 }
