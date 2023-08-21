059d349c50459a13e176c0e72eb8c36d445d6920
IGOR0SHAM <igor.sgride@yandex.ru>
2023-08-21 21:05:41 +0500

    Hibernate commit

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,14 +1,24 @@
 package jm.task.core.jdbc.util;
 
 import com.mysql.cj.jdbc.Driver;
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
     private static final String URL = "jdbc:mysql://localhost:3306/users";
     private static final String USERNAME = "sham";
     private static final String PASS = "root10";
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static SessionFactory sessionFactory;
 
     public static Connection getConnection() {
         Connection connection = null;
@@ -20,5 +30,26 @@ public class Util {
             System.err.println("Dont load driver SQL");
         }
         return connection;
+    }
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+                Properties setting = new Properties();
+                setting.put(Environment.DRIVER, DRIVER);
+                setting.put(Environment.URL, URL);
+                setting.put(Environment.USER, USERNAME);
+                setting.put(Environment.PASS, PASS);
+                configuration.setProperties(setting);
+                configuration.addAnnotatedClass(User.class);
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            } catch (Exception e) {
+                System.out.println("Проблема с созданием сессии");
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
     }
 }
